f127ea28d3f2537052c9c9cc76f20f1417e8b419
Tokio <104767426+Dayton-101@users.noreply.github.com>
2022-10-07 13:44:12 +0200

    Add files via upload

---
+++ Rust/variables.rs
@@ -0,0 +1,59 @@
+
+fn main(){
+
+    // numeros enteros con signo ( signed )
+
+    let datoenteroi1 : i8 = -255;
+
+    let datoenteroi2 : i16 = -65535;
+
+    let datoenteroi3 : i32 = -4294467295;
+
+    let datoenteroi4 : i64 = -18446744073709551615;
+
+    let datoenteroi5 : i128 = -340282366920938463463374607431768211455;
+
+
+    // numeros enteros sin signo ( unsigned )
+
+    let datodecimalu1 : u8 = 255;
+
+    let datodecimalu2 : u16 = 65535;
+
+    let datodecimalu3 : u32 = 4294467295;
+
+    let datodecimalu4 : u64 = 18446744073709551615;
+
+    let datodecimalu5 : u128 = 340282366920938463463374607431768211455;
+
+
+    // numeros decimales
+
+    let datosdecimales : f32 = 12.0;
+
+    let datosdecimales : f64 = 30.23;
+
+    // 3 bytes , caracteres
+
+    let caracter1 : char = 'a';
+
+    let caracter2 : char = 'b';
+
+    let caracter3 : char = '⋋';
+
+
+    // cadena de caracteres
+
+    let cadenacaracteres1 : String = "Esto es una cadena";
+
+    let cadenacaracteres2 : String = "Segundo cadaena de caracteres";
+
+
+    // datos booleanos
+
+    let datobooleano : bool = true;
+
+    let datosdfa : bool = false;
+
+
+}
