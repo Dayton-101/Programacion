fbc3b59de9965877ba01e588be14504328c76450
⋌ ᴛᴏᴋɪᴏ ⋌ <104767426+Dayton-101@users.noreply.github.com>
2022-10-07 18:12:42 +0200

    Add files via upload

---
+++ Rust/expresionesliterales.rs
@@ -0,0 +1,15 @@
+fn main(){
+
+    println!("{:}", 0xF23C);
+    println!("{:}", 0xFFCB);
+    println!("{:}", 0x234C);
+
+    println!("{:}", 0b0101);
+    println!("{:}", 0b0011);
+    println!("{:}", 0b1110);
+
+    println!("{:}", 0o23425);
+    println!("{:}", 0o73241);
+    println!("{:}", 0o56722);
+
+}
